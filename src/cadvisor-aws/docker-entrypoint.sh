a406d12c12fbbf3e89825ad57f8a72e3c5b8b551
Toby Clemson <tobyclemson@gmail.com>
2018-11-12 12:21:04 +0000

    Allow cadvisor port to be overridden.

--- src/cadvisor-aws/docker-entrypoint.sh
+++ src/cadvisor-aws/docker-entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-exec /cadvisor/bin/cadvisor --logtostderr
+[ "$TRACE" = "yes" ] && set -x
+set -e
+
+CADVISOR_LOGGING_OPTIONS="${CADVISOR_LOGGING_OPTIONS:- --logtostderr}"
+
+CADVISOR_PORT_OPTION=
+if [ -n "$CADVISOR_PORT" ]; then
+  CADVISOR_PORT_OPTION="--port=${CADVISOR_PORT}"
+  echo "==> Found request to listen on port '${CADVISOR_PORT}', setting port option..."
+fi
+
+exec /cadvisor/bin/cadvisor \
+    ${CADVISOR_LOGGING_OPTIONS} \
+    ${CADVISOR_PORT_OPTION}
