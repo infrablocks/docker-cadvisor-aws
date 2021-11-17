6de485b385754af84932ca0038cf8c5ff8e4c853
jonassvalin <jonassvalin@gmail.com>
2021-11-17 15:07:34 +0100

    Add additional optional flags for housekeeping_interval, disable_metrics and docker_only

--- src/cadvisor-aws/start.sh
+++ src/cadvisor-aws/start.sh
@@ -11,6 +11,27 @@ if [ -n "$CADVISOR_PORT" ]; then
   echo "==> Found request to listen on port '${CADVISOR_PORT}', setting port option..."
 fi
 
+HOUSEKEEPING_INTERVAL_OPTION=
+if [ -n "$HOUSEKEEPING_INTERVAL" ]; then
+  HOUSEKEEPING_INTERVAL_OPTION="--housekeeping_interval=${HOUSEKEEPING_INTERVAL}"
+  echo "==> Found request to set housekeeping interval '${HOUSEKEEPING_INTERVAL}', setting interval option..."
+fi
+
+DISABLE_METRICS_OPTION=
+if [ -n "$DISABLE_METRICS" ]; then
+  DISABLE_METRICS_OPTION="--disable_metrics=${DISABLE_METRICS}"
+  echo "==> Found request to disable metrics '${DISABLE_METRICS}', setting disable metrics option..."
+fi
+
+DOCKER_ONLY_OPTION=
+if [ -n "$DOCKER_ONLY" ]; then
+  DOCKER_ONLY_OPTION="--docker_only=${DOCKER_ONLY}"
+  echo "==> Found request for docker metrics only '${DOCKER_ONLY}', setting docker only option..."
+fi
+
 exec /opt/cadvisor/bin/cadvisor \
     ${CADVISOR_LOGGING_OPTIONS} \
-    ${CADVISOR_PORT_OPTION}
+    ${CADVISOR_PORT_OPTION} \
+    ${HOUSEKEEPING_INTERVAL_OPTION} \
+    ${DISABLE_METRICS_OPTION} \
+    ${DOCKER_ONLY_OPTION}
